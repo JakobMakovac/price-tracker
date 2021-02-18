26624cf4a669aba3cd49ea084354c74ad1fe7692
Jakob Makovac <jakob.makovac@gmail.com>
2021-02-18 18:21:09 +0100

    implement get list and remove for trackers

--- routes/tracker.js
+++ routes/tracker.js
@@ -42,12 +42,57 @@ router.post('/add', [jsonParser, jwtauth], function(req, res, next) {
     }
 });
 
-router.get('/list', function(req, res, next) {
-    res.status(201).send();
+router.get('/list', [jsonParser, jwtauth], function(req, res, next) {
+    if (req.userId) {
+        User.findOne({_id: req.userId}).exec(function (err, person) {
+            if (err) {
+                console.log(err);
+                res.sendStatus(500);
+            } else if (person && person.trackerIds) {
+                let queryIds = person.trackerIds.map((trackerId) => {
+                    return db.mongoose.Types.ObjectId(trackerId);
+                })
+                Tracker.find({'_id' : queryIds}, function (err, trackers) {
+                    if (err) {
+                        console.log(err);
+                        res.sendStatus(500);
+                    } else {
+                        res.json({
+                            trackers: trackers
+                        });
+                    }
+                });
+            }
+        })
+    } else {
+        res.sendStatus(401);
+    }
 });
 
-router.delete('/remove', function(req, res, next) {
-    res.status(201).send();
+router.delete('/remove', [jsonParser, jwtauth], function(req, res, next) {
+    if (req.userId && req.body.trackerId) {
+        User.findOne({_id: req.userId}).exec(function (err, person) {
+            if (err) {
+                console.log(err);
+                res.sendStatus(500);
+            } else if (person && person.trackerIds) {
+                person.trackerIds = person.trackerIds.filter((trackerId) => {
+                    return trackerId !== req.body.trackerId;
+                })
+                person.save();
+                Tracker.deleteOne({_id: req.body.trackerId}, function(err) {
+                    if (err) {
+                        console.log(err);
+                        res.sendStatus(500);
+                    } else {
+                        res.sendStatus(204);
+                    }
+                })
+            }
+        });
+    } else {
+        res.sendStatus(400);
+    }
 });
 
 module.exports = router;
